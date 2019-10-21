a70a2a194f61ef4a335cda4cc09e19fcca621d2a
David Jimenez <djresendiz1999@gmail.com>
2019-10-20 21:46:00 -0500

    FINAL

--- app/src/main/java/com/example/android/practicaparcial2/Resultados.java
+++ app/src/main/java/com/example/android/practicaparcial2/Resultados.java
@@ -15,6 +15,16 @@ public class Resultados extends AppCompatActivity {
     int punt4=0;
     int punt5=0;
     int punt6=0;
+    int frec1=0;
+    int frec2=0;
+    int frec3=0;
+    int frec4=0;
+    int frec5=0;
+    int frec6=0;
+    boolean flag3=false;
+    boolean flag4=false;
+    boolean flag5=false;
+
 
 
     @Override
@@ -39,23 +49,42 @@ public class Resultados extends AppCompatActivity {
             switch (dados[i]) {
                 case 1:
                     punt1 += 1;
+                    frec1+=1;
                     break;
                 case 2:
                     punt2 += 2;
+                    frec2+=1;
                     break;
                 case 3:
                     punt3 += 3;
+                    frec3+=1;
                     break;
                 case 4:
                     punt4 += 4;
+                    frec4+=1;
                     break;
                 case 5:
                     punt5 += 5;
+                    frec5+=1;
                     break;
                 case 6:
                     punt6+=6;
+                    frec6+=1;
                     break;
             }
+        }
+
+        if(frec1==3 || frec2==3 || frec3==3 || frec4==3 || frec5==3 || frec6==3)
+        {
+            flag3=true;
+        }
+        else if (frec1==4 || frec2==4 || frec3==4 || frec4==4 || frec5==4 || frec6==4)
+        {
+            flag4=true;
+        }
+        else if (frec1==5 || frec2==5 || frec3==5 || frec4==5 || frec5==5 || frec6==5)
+        {
+            flag5=true;
         }
 
         TextView t1 = findViewById(R.id.tv1);
@@ -65,6 +94,7 @@ public class Resultados extends AppCompatActivity {
         TextView t5 = findViewById(R.id.tv5);
         TextView t6 = findViewById(R.id.tv6);
         TextView tot = findViewById(R.id.tPuntosTotales);
+        TextView bonus = findViewById(R.id.txtBonus);
 
         t1.setText(Integer.toString(punt1));
         t2.setText(Integer.toString(punt2));
@@ -76,13 +106,23 @@ public class Resultados extends AppCompatActivity {
 
         boolean igual5, igual4, igual3;
         //CHECAR SI HAY 3, 4 O 5 IGUALES Y SUMAR PUNTOS
-        if (dados[0] == dados[1] && dados[1] == dados[2] && dados[2] == dados[3] && dados[3] == dados[4]){//CHECA SI LOS 5 SON IGUALES
+
+        if (flag5)
+        {
             puntosTotales += 40;
-            igual5=true;
+            bonus.setText("5 iguales: 40 puntos");
+
         }
-
-
-
+        else if(flag4)
+        {
+            puntosTotales += 20;
+            bonus.setText("4 iguales: 20 puntos");
+        }
+        else if(flag3)
+        {
+            puntosTotales += 10;
+            bonus.setText("3 iguales: 10 puntos");
+        }
 
         tot.setText("Puntos: "+puntosTotales);
 
